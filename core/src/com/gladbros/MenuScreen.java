2dc92659ec868fe3d7ce56104be69d3566001251
PaulNickGLAD <38428918+PaulNickGLAD@users.noreply.github.com>
2021-03-08 05:35:03 +0300

    New version of Robby with swithching between screens

--- core/src/com/gladbros/MenuScreen.java
+++ core/src/com/gladbros/MenuScreen.java
@@ -7,11 +7,19 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.gladbros.Objects.Button;
 
 public class MenuScreen implements Screen {
     public final Game game;
     SpriteBatch batch;
     Texture img;
+    public static Stage stage;
+    Button button;
+
 
     public MenuScreen(Game game) {
         this.game = game;
@@ -20,20 +28,31 @@ public class MenuScreen implements Screen {
 
     @Override
     public void show() {
+
+        stage = new Stage(new ScreenViewport());
+        Gdx.input.setInputProcessor(stage);
         batch = new SpriteBatch();
-        img = new Texture("Robby.jpg");
+        img = new Texture("mainmenu.jpg");
+        //button = new Button("start_button", 20,20);
+        Skin mySkin = new Skin(Gdx.files.internal("glassy-ui.json"));
+        ImageButton imageButton = new ImageButton(mySkin);
+        stage.addActor(imageButton);
+
     }
 
     @Override
     public void render(float delta) {
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stage.act();
+        stage.draw();
+        /**
         batch.begin();
         batch.draw(img, 0, 0);
         batch.end();
         if(Gdx.input.isKeyPressed(Input.Keys.SPACE)){
             game.setScreen(new GameScreen(this));
-        }
+        }**/
     }
 
     @Override
